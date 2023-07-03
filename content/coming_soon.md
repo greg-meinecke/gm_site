6afe9ff033b1cc2f5ca3e4a621184a52ba663e1a
_greg <gregmeinecke@gmail.com>
2023-07-02 22:10:55 -0400

    Initialize Pelican Site

---
+++ content/coming_soon.md
@@ -0,0 +1,5 @@
+Title: Site coming soon!
+Date: 2023-07-02 21:57
+Category: All
+
+I should probably do something with this domain.
---
+++ pelicanconf.py
@@ -0,0 +1,30 @@
+AUTHOR = "Greg Meinecke"
+SITENAME = "Greg Meinecke"
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = "America/New_York"
+
+DEFAULT_LANG = "en"
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = ()
+
+# Social widget
+SOCIAL = (
+    # ("You can add links in your config file", "#"),
+    # ("Another social link", "#"),
+)
+
+DEFAULT_PAGINATION = 5
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
